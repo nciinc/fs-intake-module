896d63ea921b5d140d7a9dc47949d00b9ad10b2a
cwolf1038 <cwolf123456@yahoo.com>
2017-10-26 13:34:20 -0400

    Covered final branches of nextStep()

--- frontend/src/app/trees/forests/tree-guidelines/wizard-view.component.spec.ts
+++ frontend/src/app/trees/forests/tree-guidelines/wizard-view.component.spec.ts
@@ -192,7 +192,58 @@ describe('WizardViewComponent', () => {
         'title': 'Trip planning'
     });
     component.nextStep();
+    console.log(JSON.stringify(component.sectionInfo, null, 4))
     expect(component.currentStep.step).toEqual(5);
+  });
+
+  it('should go to next step which has no subsections', () => {
+    component.forest = forest;
+    component.jumpToStep({
+        'step': 3,
+        'title': 'Tree Cutting',
+        'subsections': [
+            {
+                'step': 0,
+                'title': 'Before you cut'
+            },
+            {
+                'step': 1,
+                'title': 'When you cut'
+            },
+            {
+                'step': 2,
+                'title': 'After you cut'
+            }
+        ]
+    });
+    component.currentSubsection = component.findSubsectionStep(component.currentStep, 2);
+    component.nextStep();
+    expect(component.currentStep.step).toEqual(4);
+  });
+
+  it('should go to next step without subsections from later step, second subsection', () => {
+    component.forest = forest;
+    component.jumpToStep({
+        'step': 1,
+        'title': 'Where to Find Your Tree',
+        'subsections': [
+            {
+                'step': 0,
+                'title': 'Districts and maps'
+            },
+            {
+                'step': 1,
+                'title': 'Prohibited areas'
+            },
+            {
+                'step': 2,
+                'title': 'Places to try'
+            }
+        ]
+    });
+    component.currentSubsection = component.findSubsectionStep(component.currentStep, 1);
+    component.nextStep();
+    expect(component.currentStep.step).toEqual(1);
   });
 
 });
