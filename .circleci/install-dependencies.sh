5b4238e5262d91805a7b367a0904b258e2bf99cb
cwolf1038 <cwolf123456@yahoo.com>
2018-01-08 14:35:04 -0500

    Fixed reference in install dependencies script to run inside docker containers

--- .circleci/install-dependencies.sh
+++ .circleci/install-dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker-compose run fs-intake-frontend yarn  
-docker-compose run fs-intake-server yarn && yarn global add mocha && yarn global add nyc
+docker-compose run fs-intake-server yarn && docker-compose run fs-intake-server yarn global add mocha && docker-compose run fs-intake-server yarn global add nyc
