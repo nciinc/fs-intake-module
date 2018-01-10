6a95a45215efcb137af739f539b131865a2134c0
cwolf1038 <cwolf123456@yahoo.com>
2018-01-10 00:28:56 -0500

    Fixed run-unauthenticated e2e by setting PLATFORM env var to local by default in image, but allowing it to be overridden to put environment in a state where the e2e spec will pass

--- .circleci/run-unauthenticated-e2e.sh
+++ .circleci/run-unauthenticated-e2e.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-docker-compose run -e PLATFORM='not ci' fs-intake-frontend yarn run e2e:ci --specs=e2e/unauthenticated/check-authentication.e2e-spec.ts;
+
+#Remove running server image so one with an updated platform env var can be created
+docker-compose down
+#Rebuild with PLATFORM set to something other than local to enable test to pass
+docker-compose build --build-arg PLATFORM=NOTCI fs-intake-server 
+
+docker-compose run -e PLATFORM='not ci' fs-intake-frontend yarn run e2e:ci --environment docker --specs=e2e/unauthenticated/check-authentication.e2e-spec.ts;
 e2ereturncode=$?
 
 if [[ $e2ereturncode = 0 ]]
