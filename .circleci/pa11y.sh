7e3ed566457a5a39a7f5fd2bce4661c7069b25c1
cwolf1038 <cwolf123456@yahoo.com>
2018-03-14 15:58:07 -0400

    Added output to help diagnose

--- .circleci/pa11y.sh
+++ .circleci/pa11y.sh
@@ -1,7 +1,10 @@
+echo 'building pa11y\n\n'
 docker-compose build fs-intake-pa11y
+echo 'start application\n\n'
 docker-compose up fs-intake-pa11y &
 sleep 120
 cd ../frontend
+echo 'running pa11y\n\n'
 pa11y-ci
 pa11yreturncode=$?
 if [[ $pa11yreturncode = 0 ]]
